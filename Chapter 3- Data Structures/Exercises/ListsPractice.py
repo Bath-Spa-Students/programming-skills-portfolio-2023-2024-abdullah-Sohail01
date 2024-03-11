31b9e2c8a3036ee6779863aad35b02a7203a8a58
abdullah-Sohail01 <abdullahkh94500@gmail.com>
2024-03-11 14:44:33 +0400

    chapter3 list practice,
    Lists practice

---
+++ Chapter 3- Data Structures/Exercises/ListsPractice.py
@@ -0,0 +1,39 @@
+#append:
+
+list_1 = [2,4,6,8,10]
+list_1.append(12)
+print(list_1)
+
+#Index method:
+
+list_2 = [5,10,15,20,25,30]
+print(list_2.index(25))
+
+#Sort:
+
+list_3 = [4,8,16,12,20]
+list_3.sort()
+print(list_3)
+
+#Reverse:
+
+list_4 = [10,20,30,40,50]
+list_4.reverse()
+print(list_4)
+
+#remove:
+
+list_5 = [50,100,150,200]
+list_5.remove(150)
+print(list_5)
+
+#Min and Max:
+
+print(max(list_5))
+print(min(list_5))
+
+
+
+
+
+
