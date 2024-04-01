9f4722e3b67a4db166fa42079b59e9e365802001
abdullah-Sohail01 <abdullahkh94500@gmail.com>
2024-04-02 02:41:18 +0400

    Chapter4- All questions

---
+++ Chapter 4- Control Flow/Exercises/Exercise4.py
@@ -0,0 +1,146 @@
+
+
+
+#Q1:
+print("\n\n ---------> Question1: <---------")
+
+alien_color='green'
+
+if alien_color == 'green':
+      print("The player just earned 5 points")
+
+alien_color = 'red'
+
+if alien_color == 'green':
+      print("The player just earned 5 points.")
+
+
+
+#Q2:
+
+print("\n\n ---------> Question2: <---------")
+
+alien_color = 'red'
+
+if alien_color == 'red':
+      print("The player just earned 5 points for shooting the alien.")
+
+else:
+      print("The player just earned 10 points")    
+
+
+alien_color = 'green'
+
+if alien_color == 'red':
+      print("The player just earned 5 points for shooting the alien.")
+
+else:
+      print("The player just earned 10 points. ")    
+
+
+
+
+
+
+
+
+
+#Q3:
+
+print("\n\n ---------> Question3: <---------")
+
+# Version#1:
+
+alien_color='yellow'
+
+if alien_color == 'yellow':
+
+      print("<------ The Player earned 5 points. ------>")
+
+elif alien_color == 'green':
+      print("<------ The Player earned 10 points. ------>")
+
+else:
+      print("<------ This is not a valid color. ------>") 
+
+
+#version#2:
+
+alien_color = 'green'
+     
+if alien_color == 'yellow':
+      print("<------ The Player earned 5 points. ------>")
+
+elif alien_color == 'green':
+      print("<------ The Player earned 10 points. ------>")
+
+else:
+      print("<------ This is not a valid color. ------>") 
+
+
+#version#3:
+
+alien_color="red"
+
+if alien_color == 'yellow':
+      print("<------ The Player earned 5 points. ------>")
+
+elif alien_color == 'green':
+      print("<------ The Player earned 10 points. ------>")
+
+else:
+      print("<------ This is not a valid color. ------>") 
+
+
+
+
+
+
+
+#Q4:
+
+print("\n\n ---------> Question4: <---------")
+
+Age = 20
+
+if Age < 2:
+     print("The person is a baby.")
+
+elif Age < 4:
+     print("The person is a toddler.")
+
+elif Age < 13:
+     print("The person is a kid.")
+
+elif Age < 20:
+     print("The person is a teenager.")
+
+elif Age < 65:
+     print("The person is an adult.")
+
+else:
+     print("The person is an elder.")    
+
+
+
+#Q5:
+
+print("\n\n ---------> Question5: <---------")
+
+Fav_Fruits = ['Watermelon','Mango','Orange']
+
+if 'Watermelon' in Fav_Fruits:
+    print("I really like Watermelon.")
+
+if 'Mango' in Fav_Fruits:
+    print("I really like Mango.")
+
+if 'Orange' in Fav_Fruits:
+    print("I really like Orange.")
+
+if 'Apple' in Fav_Fruits:
+    print("I really like Apple.")
+
+if 'Banana' in Fav_Fruits:
+    print("I really like Banana.")
+
